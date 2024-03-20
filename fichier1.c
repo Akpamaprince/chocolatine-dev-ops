bbe624efcd2f9158a7d095824b41970da4a58cc2
Prince-ivan Akpama <prince-ivan.akpama@epitech.eu>
2024-03-20 18:37:00 +0100

    add to fichier1.c

---
+++ fichier1.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2024
+** fichier1.c
+** File description:
+** fichier1.c
+*/
+
+#include <stdio.h>
+
+int main() {
+    int a,b;
+    
+    printf("Bonjour");
+    return 0;
+}    
